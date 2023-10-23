7f64b675014bf6c89ff848545012f8ae16c20f67
Ruslan Kardanov <ruslan.kardanov@verimi.com>
2023-10-23 02:10:57 +0200

    regenerating dist subtree

---
+++ dist/assets/ContactsView-f409d1f9.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c}from"./index-c919deeb.js";const s={},_={class:"contacts"};function n(o,a){return t(),c("div",_," Здесь будут контакты ")}const i=e(s,[["render",n]]);export{i as default};
