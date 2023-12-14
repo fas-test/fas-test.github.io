edbf83c0be8730981f244a87aae13ee57204e0c7
Ruslan Kardanov <ruslan.kardanov@verimi.com>
2023-12-15 00:07:07 +0100

    Removed dist

--- dist/assets/ProgressView-530f1cc3.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as s,c as _}from"./index-e00dff9a.js";const o={},r={class:"progress"};function c(t,n){return s(),_("div",r,' "Прогресс" ')}const i=e(o,[["render",c]]);export{i as default};
