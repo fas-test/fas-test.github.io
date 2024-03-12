2642f60ef4ba716d6a1be56969b93500c67edf3c
Ruslan Kardanov <ruslan.kardanov@verimi.com>
2024-03-13 00:19:33 +0100

    Re-generating dist 13.03.2024

---
+++ dist/assets/ContactsView-aWpVomoF.js
@@ -0,0 +1,1 @@
+import{_ as c,o as t,c as e}from"./index-DyoQ2764.js";const o={},s={class:"contacts"};function n(a,r){return t(),e("div",s," Контакты ")}const f=c(o,[["render",n]]);export{f as default};
---
+++ dist/assets/EventsView-DsQcINWg.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as s}from"./index-DyoQ2764.js";const t={},n={class:"events"};function o(r,_){return c(),s("div",n," Мероприятия ")}const f=e(t,[["render",o]]);export{f as default};
